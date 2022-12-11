7d0d44d6e1b27067c6f3f9965242cd3bcd226d13
shatha siala <shatha.siala@gmail.com>
2022-12-11 13:34:01 +0100

    changed package and updated code

--- src/main/java/analyser/MethodAnalyser.java
+++ src/main/java/analyser/MethodAnalyser.java
@@ -1,35 +1,54 @@
 package analyser;
 
 import com.github.javaparser.JavaParser;
+import com.github.javaparser.ParseResult;
+import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.MethodDeclaration;
-import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
-import java.io.File;
-import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class MethodAnalyser {
+	
+	/**
+	 * Simple visitor implementation for visiting MethodDeclaration nodes.
+	 */
+	private static class MethodVisitor extends VoidVisitorAdapter<Object> {
 
-	public static void listMethodNames(File file) {
-
-		System.out.println(file);
-		try {
-			new VoidVisitorAdapter<Object>() {
-				@Override
-				public void visit(MethodDeclaration n, Object arg) {
-					super.visit(n, arg);
-					System.out.println(" [L " + n.getBegin() + "] " + n.getName());
-				}
-			}.visit(JavaParser.parse(file), null);
-			System.out.println(); // empty line
-		} catch (IOException e) {
-			new RuntimeException(e);
+		@Override
+		public void visit(MethodDeclaration n, Object arg) {
+			// this method will be called for all methods in this
+			// CompilationUnit, including inner class methods
+			System.out.println(n.getName());
 		}
-
 	}
 
 	public static void main(String[] args) {
-		File filename = new File("TestCode.java");
-		listMethodNames(filename);
+		
+		String filename= "TestCode.java";
+		
+		System.out.println("**** File names in file "+filename+" ****");
+
+		FileInputStream in;
+		JavaParser javaParser = new JavaParser();
+		ParseResult<CompilationUnit> pcu;
+		
+		try {
+			in = new FileInputStream(filename);
+			pcu = javaParser.parse(in);
+
+			if (pcu.isSuccessful() && pcu.getResult().isPresent()) {
+				CompilationUnit cu = pcu.getResult().get();
+				new MethodVisitor().visit(cu, null);
+			} 
+			else {
+				System.out.println("Issue，" + pcu.getProblem(0).toString());
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
